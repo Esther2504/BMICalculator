1773eabb58580c0f3733ea00bcd9f930aacc0c3a
Esther Schuitemaker <esther252000@gmail.com>
2022-07-06 16:38:42 +0200

    verbetering

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ function calculateBMI() {
     let height = document.getElementById("height").value;
 let weight = document.getElementById("weight").value;
 let BMI = weight / ((height / 100) * (height / 100))
+let BMI2 = Math.round(BMI * 10) / 10;
 let bmi = document.getElementById("bmi");
-bmi.innerText = BMI
+bmi.innerText = `Je BMI is ${BMI2}.`;
 
 }
 
