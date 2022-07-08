c6baa08da2eb3d04b350e8844d021d8a43541b86
Esther Schuitemaker <esther252000@gmail.com>
2022-07-08 08:38:37 +0200

    opmaak

--- script.js
+++ script.js
@@ -6,14 +6,13 @@ let weight = document.getElementById("weight").value;
 let BMI = weight / ((height / 100) * (height / 100))
 let BMI2 = Math.round(BMI * 10) / 10;
 let bmi = document.getElementById("bmi");
-bmi.innerText = `Je BMI is ${BMI2}.`;
 
 if (BMI < 18.5) {
-    bmi.innerText += 'Je hebt ondergewicht'
+    bmi.innerHTML += `<p>Je BMI is ${BMI2}</p><p>Je hebt ondergewicht</p>`
 } else if (BMI >= 18.5 && BMI < 25) {
-    bmi.innerText += 'Je hebt een gezond gewicht'
+    bmi.innerHTML += `<p>Je BMI is ${BMI2}</p><p>Je hebt een gezond gewicht</p>`
 } else {
-    bmi.innerText += 'Je hebt overgewicht'
+    bmi.innerHTML += `<p>Je BMI is ${BMI2}</p><p>Je hebt overgewicht</p>`
 }
 
 }
