4a3d922ae296a2619627f72f3c3121ec72827821
Esther2504 <esther252000@gmail.com>
2023-01-03 18:55:08 +0100

    styling update

--- script.js
+++ script.js
@@ -3,17 +3,26 @@
 function calculateBMI() {
     let height = document.getElementById("height").value;
 let weight = document.getElementById("weight").value;
+console.log(weight)
+
 let BMI = weight / ((height / 100) * (height / 100))
 let BMI2 = Math.round(BMI * 10) / 10;
+let description = document.getElementById("description");
 let bmi = document.getElementById("bmi");
 
 if (BMI < 18.5) {
-    bmi.innerHTML += `<p>Je BMI is ${BMI2}</p><p>Je hebt ondergewicht. Probeer aan te komen.</p>`
+    description.innerText = `<p>Je BMI is ${BMI2}</p><p>Je hebt ondergewicht. Probeer aan te komen. Een gezond BMI ligt tussen de 18.5 en 25.</p>`
 } else if (BMI >= 18.5 && BMI < 25) {
-    bmi.innerHTML += `<p>Je BMI is ${BMI2}</p><p>Je hebt een gezond gewicht.</p>`
+    description.innerText = `<p>Je BMI is ${BMI2}</p><p>Je hebt een gezond gewicht.</p>`
+} else if (BMI < 16) {
+    description.innerText = `<p>Je BMI is ${BMI2}</p><p>Je hebt ernstig ondergewicht. Probeer aan te komen.</p>`
+} else if (BMI > 30) {
+    description.innerText = `<p>Je BMI is ${BMI2}</p><p>Je hebt obesitas</p>`
 } else {
-    bmi.innerHTML += `<p>Je BMI is ${BMI2}</p><p>Je hebt overgewicht. Probeer af te vallen.</p>`
+    description.innerText = `<p>Je BMI is ${BMI2}</p><p>Je hebt overgewicht. Probeer af te vallen.</p>`
 }
+
+bmi.innerText = BMI2
 
 }
 
