bdb4577a32336a6d82fec4077281ea70e79735f2
Esther2504 <esther252000@gmail.com>
2023-01-04 15:11:46 +0100

    fullyfunctional

--- script.js
+++ script.js
@@ -3,7 +3,9 @@
 function calculateBMI() {
     let height = document.getElementById("height").value;
 let weight = document.getElementById("weight").value;
-console.log(weight)
+let white = document.querySelector(".white");
+
+white.style.opacity = 1
 
 let BMI = weight / ((height / 100) * (height / 100))
 let BMI2 = Math.round(BMI * 10) / 10;
@@ -11,15 +13,13 @@ let description = document.getElementById("description");
 let bmi = document.getElementById("bmi");
 
 if (BMI < 18.5) {
-    description.innerText = `<p>Je BMI is ${BMI2}</p><p>Je hebt ondergewicht. Probeer aan te komen. Een gezond BMI ligt tussen de 18.5 en 25.</p>`
+    description.innerText = `Je hebt ondergewicht. Een gezond BMI ligt tussen de 18.5 en 25.`
 } else if (BMI >= 18.5 && BMI < 25) {
-    description.innerText = `<p>Je BMI is ${BMI2}</p><p>Je hebt een gezond gewicht.</p>`
-} else if (BMI < 16) {
-    description.innerText = `<p>Je BMI is ${BMI2}</p><p>Je hebt ernstig ondergewicht. Probeer aan te komen.</p>`
-} else if (BMI > 30) {
-    description.innerText = `<p>Je BMI is ${BMI2}</p><p>Je hebt obesitas</p>`
-} else {
-    description.innerText = `<p>Je BMI is ${BMI2}</p><p>Je hebt overgewicht. Probeer af te vallen.</p>`
+    description.innerText = `Je hebt een gezond gewicht. Een gezond BMI ligt tussen de 18.5 en 25.`
+} else if (BMI >= 25 && BMI < 30) {
+    description.innerText = `Je hebt overgewicht. Een gezond BMI ligt tussen de 18.5 en 25.`
+} else if (BMI >= 30) {
+    description.innerText = `Je hebt obesitas. Een gezond BMI ligt tussen de 18.5 en 25.`
 }
 
 bmi.innerText = BMI2
